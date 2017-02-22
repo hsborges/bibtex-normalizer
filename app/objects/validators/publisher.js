64b8bf75a0602ffed62735ca3a087061d3565112
Paulo Carvalho <paulocarvalho@dcc.ufmg.br>
2017-02-22 15:44:23 -0300

    Validators for all required attributes

---
+++ app/objects/validators/publisher.js
@@ -0,0 +1,12 @@
+class PublisherValidator {
+  constructor() { }
+
+  validate(value) {
+    return {
+      isValid: /^\S/.test(value),
+      message: 'Must consist of literals (a-z, A-Z).'
+    };
+  }
+}
+
+export default (new PublisherValidator());
