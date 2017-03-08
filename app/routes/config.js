db418ac513d1c0970ee49e46270995b06cb6c37d
Paulo Carvalho <paulocarvalho@dcc.ufmg.br>
2017-03-08 15:35:14 -0300

    Refactoring iss #11 : attributes and page design

--- app/routes/config.js
+++ app/routes/config.js
@@ -1,5 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  formatter: Ember.inject.service(),
 
+  model() {
+    const entriesObjects = {
+    	"article": {
+    		"required": ["author", "title", "journal", "year"],
+    		"optional": ["volume", "number", "pages", "month", "note", "key"]
+    	},
+    	"book": {
+    		"required": ["author", "editor", "title", "publisher", "year"],
+    		"optional": ["volume", "series", "address", "edition", "month", "note", "key"]
+    	},
+    	"inproceedings": {
+    		"required": ["author", "title", "booktitle", "year"],
+    		"optional": ["editor", "pages", "organization", "publisher", "address", "month", "note", "key"]
+    	},
+    	"conference": {
+    		"required": ["author", "title", "booktitle", "year"],
+    		"optional": ["editor", "pages", "organization", "publisher", "address", "month", "note", "key"]
+    	}
+    };
+    return entriesObjects;
+  },
+
+  actions: {
+    configure() {
+      console.log(this.get('teste'));
+    }
+  }
 });
