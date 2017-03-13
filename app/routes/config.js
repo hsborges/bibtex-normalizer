da686302b456ff16b625e2832fe2380cd0318b3f
Paulo Carvalho <paulocarvalho@dcc.ufmg.br>
2017-03-13 12:32:49 -0300

    #11: Check every checked attributefrom config router

--- app/routes/config.js
+++ app/routes/config.js
@@ -2,32 +2,40 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   formatter: Ember.inject.service(),
+  entriesObjects: {
+    "article": {
+      "required": ["author", "title", "journal", "year"],
+      "optional": ["volume", "number", "pages", "month", "note", "key"]
+    },
+    "book": {
+      "required": ["author", "editor", "title", "publisher", "year"],
+      "optional": ["volume", "series", "address", "edition", "month", "note", "key"]
+    },
+    "inproceedings": {
+      "required": ["author", "title", "booktitle", "year"],
+      "optional": ["editor", "pages", "organization", "publisher", "address", "month", "note", "key"]
+    },
+    "conference": {
+      "required": ["author", "title", "booktitle", "year"],
+      "optional": ["editor", "pages", "organization", "publisher", "address", "month", "note", "key"]
+    }
+  },
 
   model() {
-    const entriesObjects = {
-    	"article": {
-    		"required": ["author", "title", "journal", "year"],
-    		"optional": ["volume", "number", "pages", "month", "note", "key"]
-    	},
-    	"book": {
-    		"required": ["author", "editor", "title", "publisher", "year"],
-    		"optional": ["volume", "series", "address", "edition", "month", "note", "key"]
-    	},
-    	"inproceedings": {
-    		"required": ["author", "title", "booktitle", "year"],
-    		"optional": ["editor", "pages", "organization", "publisher", "address", "month", "note", "key"]
-    	},
-    	"conference": {
-    		"required": ["author", "title", "booktitle", "year"],
-    		"optional": ["editor", "pages", "organization", "publisher", "address", "month", "note", "key"]
-    	}
-    };
-    return entriesObjects;
+    return this.get('entriesObjects');
   },
 
   actions: {
     configure() {
-      console.log(Ember.$('.article').get('checked'));
+      for(let entry in this.get('entriesObjects')) {
+        let indexEntry = this.get('entriesObjects')[entry];
+
+        for(let i=0; i < indexEntry.optional.length; i++) {
+          let idOptional = `#input-${entry}-${indexEntry.optional[i]}`;
+          console.log(`${$(idOptional).is(':checked')}`);
+        }
+
+      }
     }
   }
 });
