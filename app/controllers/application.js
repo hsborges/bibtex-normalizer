a1816609230f0bd3e5e3e72fece6fde9d430a3bb
hsborges <hudsonsilbor@gmail.com>
2017-07-13 17:37:37 -0300

    interface improve

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -11,5 +11,16 @@ export default Ember.Controller.extend({
   init(){
     this._super(...arguments);
     this.get('configuration').setup();
+
+    Ember.run.schedule("afterRender", this, function() {
+      const self = this;
+      Ember.$(window).on('scroll', function () {
+        if(!self.get('index') && Ember.$(window).scrollTop()) {
+          Ember.$('.app-header').addClass('app-header-fixed')
+        } else {
+          Ember.$('.app-header').removeClass('app-header-fixed')
+        }
+      });
+    });
   }
 });
