ad6f8135fb2c82e3264a189747b3835e7c2cac1d
Hudson Silva Borges <hudsonsilbor@gmail.com>
2022-04-26 19:52:12 -0400

    feat(analytics): add google analitics

---
+++ lib/gtag.ts
@@ -0,0 +1,22 @@
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url: string) => {
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = (params: {
+  action: string;
+  category: string;
+  label: string;
+  value: string;
+}) => {
+  window.gtag('event', params.action, {
+    event_category: params.category,
+    event_label: params.label,
+    value: params.value,
+  });
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,33 @@
  */
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 import { styled } from '@stitches/react';
 
+import Analytics from '../components/analytics';
 import BetaBadge from '../components/beta-badge';
 import Footer from '../components/footer';
 import Header from '../components/header';
+import * as gtag from '../lib/gtag';
 import { ConfigProvider } from '../providers/ConfigProvider';
 import { EditorConfigProvider } from '../providers/EditorProvider';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   const Grid = styled('div', {
     display: 'flex',
     flexDirection: 'column',
@@ -24,21 +40,24 @@ export default function App({ Component, pageProps }: AppProps) {
   const MainContent = styled('div', { flexGrow: 1 });
 
   return (
-    <ConfigProvider>
-      <EditorConfigProvider>
-        <Head>
-          <title>Bibtex Normalizer</title>
-          <link rel="shortcut icon" href="/images/logo.png" />
-        </Head>
-        <Grid>
-          <BetaBadge />
-          <Header />
-          <MainContent>
-            <Component {...pageProps} />
-          </MainContent>
-          <Footer />
-        </Grid>
-      </EditorConfigProvider>
-    </ConfigProvider>
+    <>
+      <ConfigProvider>
+        <EditorConfigProvider>
+          <Head>
+            <title>Bibtex Normalizer</title>
+            <link rel="shortcut icon" href="/images/logo.png" />
+          </Head>
+          <Grid>
+            <BetaBadge />
+            <Header />
+            <MainContent>
+              <Component {...pageProps} />
+            </MainContent>
+            <Footer />
+          </Grid>
+        </EditorConfigProvider>
+      </ConfigProvider>
+      <Analytics />
+    </>
   );
 }
