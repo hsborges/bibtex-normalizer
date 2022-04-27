2933134d9cd623e43dac8f2b59cfacc51c7f918e
Hudson SIlva Borges <hudsonsilbor@gmail.com>
2022-04-26 20:15:14 -0400

    Add Google Analytics (#38)
    * feat(analytics): add google analitics

---
+++ components/analytics.tsx
@@ -0,0 +1,28 @@
+import Script from 'next/script';
+
+import { GA_TRACKING_ID } from '../lib/gtag';
+
+export default function Analytics() {
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="bibtex-normalizer-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+    </>
+  );
+}
