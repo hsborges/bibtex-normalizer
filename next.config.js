fc197d6f8f8d50a6acb976ec95d1cba76e6f0820
Hudson Silva Borges <hudsonsilbor@gmail.com>
2022-04-26 23:20:35 -0400

    ci(gh-pages): attempt to fix deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/bibtex-normalizer' : '',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
