41f085d12c395c729ac536fcf04e69cf7c83f6d2
Hudson Silva Borges <hudsonsilbor@gmail.com>
2022-05-03 17:50:59 -0400

    refactor: move viewport meta from _document to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -64,6 +64,8 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta property="og:image:height" content="400" />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="en_US" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
       </Head>
       <ConfigProvider>
         <EditorConfigProvider>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
       </Head>
       <body>
