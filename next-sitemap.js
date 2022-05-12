4727d38f90a2b8d401bfec0832143f48f4f15977
Hudson Silva Borges <hudsonsilbor@gmail.com>
2022-05-12 11:41:19 -0400

    build: add sitemap and robot generator tool

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: 'https://hsborges.github.io/bibtex-normalizer',
+  outDir: 'bibtex-normalizer',
+  generateRobotsTxt: true,
+  exclude: ['/editor/settings*'],
+};
